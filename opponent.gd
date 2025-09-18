9862fa4f572fac0cf219f14444d6d09e9bdc09b5
Aidan Rogers <143754636+airo4858@users.noreply.github.com>
2025-09-18 14:12:12 -0600

    finished???

--- opponent.gd
+++ opponent.gd
@@ -7,10 +7,10 @@ func _physics_process(delta: float) -> void:
 	position.x = clamp(position.x,0,432)
 	opp_movement = clamp(opp_movement,0.7,0.8)
 	var ball_x = get_parent().get_node("Ball").position.x
-	if (position.x == ball_x):
-		opp_speed = 0
 	if (position.x > ball_x):
 		opp_speed = -opp_movement
+		await get_tree().create_timer(0.5).timeout
 	if (position.x < ball_x):
 		opp_speed = opp_movement
+		await get_tree().create_timer(0.5).timeout
 	position.x += opp_speed
